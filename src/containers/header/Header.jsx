282fb8a88ec3f529a97bbea2b08cc693a371b174
David Nguyen <42897656+ibdavid7@users.noreply.github.com>
2023-02-11 00:52:59 +0000

    Header.jsx and header.css implemented

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
                     <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
                 <div className='gpt3__header-image'>
-                    <img src={air} alt="ai" />
+                    <img src={ai} alt="ai" />
                 </div>
             </div>
             Header
